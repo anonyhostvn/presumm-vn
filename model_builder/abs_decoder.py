5f974fde29a326eb9ad0db05b1cba59855926533
Long Nguyen <anonyhost@gmail.com>
2021-12-21 11:50:07 +0700

    turn tgt_mask to boolean

--- model_builder/abs_decoder.py
+++ model_builder/abs_decoder.py
@@ -8,8 +8,7 @@ class AbsDecoder(nn.Module):
         super(AbsDecoder, self).__init__()
         decoder_layer = nn.TransformerDecoderLayer(batch_first=True, d_model=MODEL_DIM, nhead=n_head, norm_first=True)
         self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=n_decoder_block)
-        self.device = nn.Parameter(torch.empty(0)).device
-        self.tgt_mask = torch.triu(torch.ones(size=(MAX_SEQ_LENGTH, MAX_SEQ_LENGTH)), diagonal=1).T.to(self.device)
+        self.tgt_mask = torch.triu(torch.ones(size=(MAX_SEQ_LENGTH, MAX_SEQ_LENGTH)), diagonal=1).T.bool()
 
     # tgt: đầu vào của decoder (batch_size * tgt_seq_len * embed_dim)
     # memory: embed của input sequence (batch_size * src_seq_len * embed_dim)
@@ -17,6 +16,7 @@ class AbsDecoder(nn.Module):
     # memory_key_padding_mask: mask các token padding của src (batch_size * src_seq_len)
     # tgt_mask: attention mask của decoder (để mô hình không nhìn được token ở tương lai)
     def forward(self, tgt: Tensor, memory: Tensor, tgt_key_padding_mask: Tensor, memory_key_padding_mask: Tensor):
+        self.tgt_mask = self.tgt_mask().to(tgt.device)
         out = self.transformer_decoder(tgt=tgt, memory=memory,
                                        tgt_key_padding_mask=tgt_key_padding_mask,
                                        memory_key_padding_mask=memory_key_padding_mask,
